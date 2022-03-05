a94d5e4f79a7b6817297fdee1301126f03c61e44
purplevarun <purplevarun@gmail.com>
2022-03-05 23:00:46 +0530

    keyboard size reduced

--- src/components/Keyboard.js
+++ src/components/Keyboard.js
@@ -32,50 +32,108 @@ const Keyboard = () => {
 	return (
 		<div className="keyboard">
 			<VStack spacing="0">
-				<HStack p="1" spacing="3">
-					<Button onClick={() => handleCharacter("Q")}>Q</Button>
-					<Button onClick={() => handleCharacter("W")}>W</Button>
-					<Button onClick={() => handleCharacter("E")}>E</Button>
-					<Button onClick={() => handleCharacter("R")}>R</Button>
-					<Button onClick={() => handleCharacter("T")}>T</Button>
-					<Button onClick={() => handleCharacter("Y")}>Y</Button>
-					<Button onClick={() => handleCharacter("U")}>U</Button>
-					<Button onClick={() => handleCharacter("I")}>I</Button>
-					<Button onClick={() => handleCharacter("O")}>O</Button>
-					<Button onClick={() => handleCharacter("P")}>P</Button>
+				<HStack p="1" spacing="1.5">
+					<Button onClick={() => handleCharacter("Q")} size="xs">
+						Q
+					</Button>
+					<Button onClick={() => handleCharacter("W")} size="xs">
+						W
+					</Button>
+					<Button onClick={() => handleCharacter("E")} size="xs">
+						E
+					</Button>
+					<Button onClick={() => handleCharacter("R")} size="xs">
+						R
+					</Button>
+					<Button onClick={() => handleCharacter("T")} size="xs">
+						T
+					</Button>
+					<Button onClick={() => handleCharacter("Y")} size="xs">
+						Y
+					</Button>
+					<Button onClick={() => handleCharacter("U")} size="xs">
+						U
+					</Button>
+					<Button onClick={() => handleCharacter("I")} size="xs">
+						I
+					</Button>
+					<Button onClick={() => handleCharacter("O")} size="xs">
+						O
+					</Button>
+					<Button onClick={() => handleCharacter("P")} size="xs">
+						P
+					</Button>
 					<IconButton
 						icon={<TiBackspaceOutline />}
-						size="lg"
+						size="sm"
 						onClick={() => handleBackspace()}
 					/>
 				</HStack>
-				<HStack p="1" spacing="3">
-					<Button onClick={() => handleCharacter("A")}>A</Button>
-					<Button onClick={() => handleCharacter("S")}>S</Button>
-					<Button onClick={() => handleCharacter("D")}>D</Button>
-					<Button onClick={() => handleCharacter("F")}>F</Button>
-					<Button onClick={() => handleCharacter("G")}>G</Button>
-					<Button onClick={() => handleCharacter("H")}>H</Button>
-					<Button onClick={() => handleCharacter("J")}>J</Button>
-					<Button onClick={() => handleCharacter("K")}>K</Button>
-					<Button onClick={() => handleCharacter("L")}>L</Button>
+				<HStack p="1" spacing="1.5">
+					<Button onClick={() => handleCharacter("A")} size="xs">
+						A
+					</Button>
+					<Button onClick={() => handleCharacter("S")} size="xs">
+						S
+					</Button>
+					<Button onClick={() => handleCharacter("D")} size="xs">
+						D
+					</Button>
+					<Button onClick={() => handleCharacter("F")} size="xs">
+						F
+					</Button>
+					<Button onClick={() => handleCharacter("G")} size="xs">
+						G
+					</Button>
+					<Button onClick={() => handleCharacter("H")} size="xs">
+						H
+					</Button>
+					<Button onClick={() => handleCharacter("J")} size="xs">
+						J
+					</Button>
+					<Button onClick={() => handleCharacter("K")} size="xs">
+						K
+					</Button>
+					<Button onClick={() => handleCharacter("L")} size="xs">
+						L
+					</Button>
 					<IconButton
 						icon={<AiOutlineEnter />}
-						size="lg"
+						size="sm"
 						onClick={() => handleEnter()}
 					/>
 				</HStack>
-				<HStack p="1" spacing="3">
-					<Button onClick={() => handleCharacter("Q")}>Q</Button>
-					<Button onClick={() => handleCharacter("W")}>W</Button>
-					<Button onClick={() => handleCharacter("E")}>E</Button>
-					<Button onClick={() => handleCharacter("R")}>R</Button>
-					<Button onClick={() => handleCharacter("T")}>T</Button>
-					<Button onClick={() => handleCharacter("Y")}>Y</Button>
-					<Button onClick={() => handleCharacter("U")}>U</Button>
-					<Button onClick={() => handleCharacter("I")}>I</Button>
-					<Button onClick={() => handleCharacter("O")}>O</Button>
-					<Button onClick={() => handleCharacter("P")}>P</Button>
+				<HStack p="1" spacing="1.5">
+					<Button onClick={() => handleCharacter("Q")} size="xs">
+						Q
+					</Button>
+					<Button onClick={() => handleCharacter("W")} size="xs">
+						W
+					</Button>
+					<Button onClick={() => handleCharacter("E")} size="xs">
+						E
+					</Button>
+					<Button onClick={() => handleCharacter("R")} size="xs">
+						R
+					</Button>
+					<Button onClick={() => handleCharacter("T")} size="xs">
+						T
+					</Button>
+					<Button onClick={() => handleCharacter("Y")} size="xs">
+						Y
+					</Button>
+					<Button onClick={() => handleCharacter("U")} size="xs">
+						U
+					</Button>
+					<Button onClick={() => handleCharacter("I")} size="xs">
+						I
+					</Button>
+					<Button onClick={() => handleCharacter("O")} size="xs">
+						O
+					</Button>
+					<Button onClick={() => handleCharacter("P")} size="xs">
+						P
+					</Button>
 				</HStack>
 			</VStack>
 		</div>
