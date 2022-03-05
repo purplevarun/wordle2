436b47de5841405b5590359860e21914013ef4d2
purplevarun <purplevarun@gmail.com>
2022-03-05 23:36:02 +0530

    Created Separate Wordboxes for each difficulty

---
+++ src/components/WordBoxes/WordBoxStandard.js
@@ -0,0 +1,39 @@
+import { HStack, Input, VStack } from "@chakra-ui/react";
+import React from "react";
+import "./../../styles/WordBox.css";
+
+const WordBoxStandard = () => {
+	return (
+		<div className="wordbox">
+			<VStack spacing={4} flex={1} justify="center" ml="2" mr={2}>
+				<HStack spacing={4}>
+					<Input></Input>
+					<Input></Input>
+					<Input></Input>
+					<Input></Input>
+				</HStack>
+				<HStack spacing={4}>
+					<Input></Input>
+					<Input></Input>
+					<Input></Input>
+					<Input></Input>
+				</HStack>
+				<HStack spacing={4}>
+					<Input></Input>
+					<Input></Input>
+					<Input></Input>
+					<Input></Input>
+				</HStack>
+				<HStack spacing={4}>
+					<Input></Input>
+					<Input></Input>
+					<Input></Input>
+					<Input></Input>
+				</HStack>
+			</VStack>
+			{/* <h1>word length = {wordLength}</h1> */}
+		</div>
+	);
+};
+
+export default WordBoxStandard;
--- src/routes/Game.js
+++ src/routes/Game.js
@@ -3,11 +3,15 @@ import React, { useEffect, useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import Keyboard from "../components/Keyboard";
 import "./../styles/Game.css";
+import WordBoxNoob from "./../components/WordBoxes/WordBoxNoob";
+import WordBoxOriginal from "./../components/WordBoxes/WordBoxOriginal";
+import WordBoxStandard from "./../components/WordBoxes/WordBoxStandard";
 const Game = () => {
 	const location = useLocation();
 	const navigate = useNavigate();
 	const [pageLoaded, setPageLoaded] = useState(false);
 	const [currLevel, setCurrLevel] = useState(null);
+	const [wordLength, setWordLength] = useState(-1);
 	useEffect(() => {
 		if (pageLoaded === false) {
 			if (location.state && location.state.level) {
@@ -17,14 +21,30 @@ const Game = () => {
 			}
 		}
 		setPageLoaded(true);
-	}, [location.state, navigate, pageLoaded, setPageLoaded]);
+		if (currLevel === "Noob") setWordLength(3);
+		if (currLevel === "Standard") setWordLength(4);
+		if (currLevel === "Original") setWordLength(5);
+	}, [currLevel, location.state, navigate, pageLoaded, setPageLoaded]);
 	return (
 		<div>
 			<div className="levelindicator">
 				<Badge colorScheme={"teal"}>{currLevel}</Badge>
 			</div>
 			<div className="game">
-				<Keyboard />
+				<div className="wordbox">
+					{wordLength === 3 ? (
+						<WordBoxNoob />
+					) : wordLength === 4 ? (
+						<WordBoxStandard />
+					) : wordLength === 5 ? (
+						<WordBoxOriginal />
+					) : (
+						""
+					)}
+				</div>
+				<div className="keyboard">
+					<Keyboard />
+				</div>
 			</div>
 		</div>
 	);
